059a80d09557e8a82996269e99de6be3385d7a70
Alponcho1 <70652796+Alponcho1@users.noreply.github.com>
2020-09-10 10:07:07 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onPinPressed(TouchPin.P0, function () {
+    volume_sphere = 4 / 3 * pi * ratio ** 3
+    basic.showNumber(volume_sphere)
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+let volume_sphere = 0
+let pi = 0
+let ratio = 0
+ratio = 10
+pi = 3.1416
+basic.forever(function () {
+	
+})
